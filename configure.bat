8057f0f3ccdf72a606862b68301a22f2b306476c
Brad Roepstorff <brad.roepstorff@gmail.com>
2020-02-02 14:46:25 -0700

    figured out headers in xcode

--- configure.bat
+++ configure.bat
@@ -1,6 +1,7 @@
 pushd %~dp0%
 if not exist build mkdir build
 cd build
+rem add --trace-expand to debug
 cmake ..
 popd
 exit /b 0
--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p build
 pushd build
+#add --trace-expand to debug
 cmake -G "Xcode" ..
 popd
 exit 0
