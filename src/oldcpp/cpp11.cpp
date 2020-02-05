b98a6deda1fbc6844b823aee6bd70778e2935f38
Brad Roepstorff <brad.roepstorff@gmail.com>
2020-02-05 11:43:54 -0700

    code cleanup, add cpp11 char16/32_t and oldcpp int8/16/32_t etc

--- src/oldcpp/cpp11.cpp
+++ src/oldcpp/cpp11.cpp
@@ -11,18 +11,29 @@ void TestCpp11Features()
     using namespace std;
     cout << "======= cpp11 features ==========\n";
     
-    //todo
-    auto f = 1.323f;
-    auto* pF = &f;
-    cout << "Auto: " << f << " " << *pF << endl;
-    //todo verify type: something like: cout << "Auto typeof: " << typeof(float) << endl;
+    //auto
+    {
+        auto f = 1.323f;
+        auto* pF = &f;
+        cout << "Auto: " << f << " " << *pF << endl;
+        //todo verify type: something like: cout << "Auto typeof: " << typeof(float) << endl;
+    }
     
+    //constexpr
     {
         constexpr int a = 3;
         constexpr int b = 5;
         constexpr const int aPlusB = a + b;
         static_assert(aPlusB == 8, "");
         //todo constexpr if ?
+    }
+    
+    //char16_t, char32_t
+    {
+        char16_t c16 = 'a';
+        char32_t c32 = 'b';
+        static_assert(sizeof(c16) == 2);
+        static_assert(sizeof(c32) == 4);
     }
     
     cout << endl;
--- src/oldcpp/oldcpp.cpp
+++ src/oldcpp/oldcpp.cpp
@@ -70,6 +70,16 @@ void TestOldCppFeatures()
     
     using namespace std;
     
+    //built-in types
+    {
+        static_assert(sizeof(int8_t) == 1);
+        static_assert(sizeof(int16_t) == 2);
+        static_assert(sizeof(int32_t) == 4);
+        static_assert(sizeof(uint8_t) == 1);
+        static_assert(sizeof(uint16_t) == 2);
+        static_assert(sizeof(uint32_t) == 4);        
+    }
+    
     //Functions
     {
         float f = 1;
